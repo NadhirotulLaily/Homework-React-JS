a7d283f18c40c1efe6868a378f39f49575114efd
NadhirotulLaily <nadhirotullaily1207@gmail.com>
2023-11-07 21:56:55 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,162 @@
+
+import * as React from 'react';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import { Box, Button, ChakraProvider, Flex, Text, VStack } from '@chakra-ui/react';
+
+
+const ticTacToe = createSlice({
+  name: 'ticTacToe',
+  initialState: {
+    squares: Array(9).fill(null),
+    currentStep: 0,
+    winner: null,
+    nextValue: 'X',
+    status: 'Next player: X',
+  },
+  reducers: {
+    selectSquare(state, action) {
+      if (!state.winner && !state.squares[action.payload]) {
+        const newSquares = [...state.squares];
+        newSquares[action.payload] = calculateNextValue(state.squares);
+        const winner = calculateWinner(newSquares);
+        const nextValue = calculateNextValue(newSquares);
+        const status = calculateStatus(winner, newSquares, nextValue);
+        return {
+          squares: newSquares,
+          winner,
+          nextValue,
+          status
+        };
+      }
+    },
+    restart(state) {
+      const newSquares = Array(9).fill(null);
+      const winner = calculateWinner(newSquares);
+      const nextValue = calculateNextValue(newSquares);
+      const status = calculateStatus(winner, newSquares, nextValue);
+      return {
+        squares: newSquares,
+        winner,
+        nextValue,
+        status,
+      };
+    },
+  },
+});
+
+export const { selectSquare, restart, jumpToMove } = ticTacToe.actions;
+
+const store = configureStore({
+  reducer: ticTacToe.reducer,
+});
+
+function Board() {
+  const { status, squares } = useSelector(state => state);
+  const dispatch = useDispatch();
+  function selectSquareHandler(squareIndex) {
+    dispatch(selectSquare(squareIndex));
+  }
+  
+  function renderSquare(i) {
+    return (
+      <Button
+        w='100px'
+        h='100px'
+        variant="outline"
+        borderWidth="2px"
+        borderColor="blue.200"
+        onClick={() => selectSquareHandler(i)}>
+        {squares[i]}
+      </Button>
+    );
+  }
+
+  return (
+    <VStack mt={4}>
+      <Text fontSize="4xl" fontWeight="bold" mb={2}>
+        {status}
+      </Text>
+      <Flex >
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </Flex>
+      <Flex >
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </Flex>
+      <Flex >
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </Flex>
+      
+      </VStack>
+  );
+}
+
+function Game() {
+  const dispatch = useDispatch();
+  function handleRestart() {
+    dispatch(restart());
+  }
+  return (
+    <Box bg="pink.100" minH="100vh" p={10}>
+      <Box maxW="md" mx="auto" bg="white" p={2} borderRadius="lg">
+        <Board />
+        <Button size="md" onClick={handleRestart} mt={4} colorScheme="teal">
+          restart
+        </Button>
+      </Box>
+    </Box>
+  );
+}
+
+// eslint-disable-next-line no-unused-vars
+function calculateStatus(winner, squares, nextValue) {
+  return winner
+    ? `Winner: ${winner}`
+    : squares.every(Boolean)
+      ? `Scratch: Cat's game`
+      : `Next player: ${nextValue}`;
+}
+
+// eslint-disable-next-line no-unused-vars
+function calculateNextValue(squares) {
+  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
+}
+
+// eslint-disable-next-line no-unused-vars
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Provider store={store}>
+        <Game />
+      </Provider>
+    </ChakraProvider>
+  );
+}
+
+export default App;
